605cb2c65dac62853cf3764b7c7888c4aefda86b
buscanopaul <buscano.paul@gmail.com>
2021-11-13 20:29:45 +0800

    Get Single product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -17,7 +17,24 @@ exports.getProducts = async (req, res, next) => {
 
     res.status(200).json({
         success: true,
-        count: products.length,
+        count: products.length, 
         products
+    })
+}
+
+// Get single product details => /api/v1/product/:id
+exports.getSingleProduct = async (req, res, next) => {
+    const product = await Product.findById(req.params.id)
+
+    if(!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found.'
+        })
+    }
+
+    res.status(200).json({
+        success: true,
+        product
     })
 }
