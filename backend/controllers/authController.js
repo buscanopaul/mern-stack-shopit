513d44a41fc4185af5d10f9727292c1a4ae92b36
buscanopaul <buscano.paul@gmail.com>
2021-11-16 21:42:24 +0800

    Encrypted password after register

---
+++ backend/controllers/authController.js
@@ -0,0 +1,23 @@
+const User = require('../models/user')
+const ErrorHandler = require('../utils/errorHandler')
+const catchAsyncErrors = require('../middlewares/catchAsyncErrors')
+
+// Register a user => /api/v1/register
+exports.registerUser = catchAsyncErrors( async (req, res, next) => {
+    const { name, email, password } = req.body
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+        avatar: {
+            public_id: 'avatars/alex-suprun-ZHvM3XIOHoE-unsplash_jqug40',
+            url: 'https://res.cloudinary.com/eastwest/image/upload/v1637069163/avatars/alex-suprun-ZHvM3XIOHoE-unsplash_jqug40.jpg'
+        }
+    }) 
+
+    res.status(201).json({
+        success: true,
+        user
+    })
+})
