988d25f5760c367910a604fe28ea826c3e4cd44b
buscanopaul <buscano.paul@gmail.com>
2021-11-18 20:23:56 +0800

    Adds Update user profile

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { registerUser, loginUser, logout, forgotPassword, resetPassword, getUserProfile, updatePassword } = require('../controllers/authController')
+const { registerUser, loginUser, logout, forgotPassword, resetPassword, getUserProfile, updatePassword, updateProfile } = require('../controllers/authController')
 
 const { isAuthenticatedUser } = require('../middlewares/auth')
 
@@ -15,5 +15,6 @@ router.route('/logout').get(logout)
 
 router.route('/me').get(isAuthenticatedUser, getUserProfile)
 router.route('/password/update').put(isAuthenticatedUser, updatePassword)
+router.route('/me/update').put(isAuthenticatedUser, updateProfile)
 
 module.exports = router;
