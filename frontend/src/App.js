7260857dcc88e0c727a01fc78a337818141b3222
buscanopaul <buscano.paul@gmail.com>
2021-11-20 22:14:40 +0800

    Adds ShopIT homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
+import Home from './components/Home'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <h1>Shop IT</h1>
+      <Home />
       <Footer />
     </div>
   );
---
+++ frontend/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="container container-fluid"> 
+            <h1 id="products_heading">Latest Products</h1>
+        </div>
+    )
+}
+
+export default Home
