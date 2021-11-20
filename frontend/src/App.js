970f49124758de4b53677dae5a32fdad25fa451f
buscanopaul <buscano.paul@gmail.com>
2021-11-20 22:26:39 +0800

    Adds Implement react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,5 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
 import Home from './components/Home'
@@ -6,11 +8,15 @@ import './App.css';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <Header />
-      <Home />
+    <Header />
+      <Routes>
+        <Route path="/" element={<Home />} exact />
+      </Routes>
       <Footer />
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
